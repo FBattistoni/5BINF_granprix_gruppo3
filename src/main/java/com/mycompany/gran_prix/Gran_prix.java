24a694de1a6d211cba517a8cb2bf929570bb19fb
KartaC <152143121+KartaC@users.noreply.github.com>
2024-02-10 19:46:33 +0100

    Karta commit
    Ho aggiunto il menu di selezione per scegliere di accedere o registrarsi, e sviluppato un pò il prototipo di struttura dei metodi, ecc...

--- src/main/java/com/mycompany/gran_prix/Gran_prix.java
+++ src/main/java/com/mycompany/gran_prix/Gran_prix.java
@@ -1,8 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.gran_prix;
+
+import java.util.Scanner;
 
 /**
  *
@@ -11,7 +12,63 @@ package com.mycompany.gran_prix;
 public class Gran_prix {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //prova
+        /*
+        *   Variabili, oggetti, ecc...
+         */
+        Scanner sc = new Scanner(System.in);
+        String nomeGiocatore;
+        String email;
+        String password;
+
+        /*  
+     *   Menu di scelta dove il giocatore deve registrarsi o accedere, creare 
+     *   un file con le credenziali, dove in caso verranno salvate, nel caso 
+     *   di accedere verrà fatto un check se le credenziali corrispondono.
+     *   L'utente immetterà la scelta e in base al suo input l'if
+     *   conterrà i metodi e codice al numero che corrispondono al menu.
+         */
+        short scelta = 0;
+        boolean successo = false;
+        while (successo == false) {
+            System.out.println("1. Registrati");
+            System.out.println("2. Accedi");
+
+            scelta = sc.nextShort();
+
+            if (scelta == 1) {
+                successo = true;
+                registrazione();
+            } else if (scelta == 2) {
+                successo = true;
+                accesso();
+            } else {
+                System.out.println("Opzione non corretta. ");
+            }
+        }
     }
+
+    /*
+     *   Metodo per la registrazione del giocatore
+     */
+    public static void registrazione() {
+        System.out.println("Scegli la tua email: ");
+        /*Scanner*/
+        System.out.println("Scegli la tua password: ");
+        /*Scanner*/
+        System.out.println("Scegli il tuo nome: ");
+        /*Scanner*/
+    }
+
+    /*
+     *   Metodo per l'accesso del giocatore
+     */
+    public static void accesso() {
+        System.out.println("Immetti il tuo nome: ");
+        /*Scanner*/
+        System.out.println("Immetti la tua email: ");
+        /*Scanner*/
+        System.out.println("Immetti la tua password: ");
+        /*Scanner*/
+    }
+
 }
