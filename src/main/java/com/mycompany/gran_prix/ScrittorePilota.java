80f91831129abc3aa13dec2f7c1830fdb6a74933
Marche05 <152077304+Marche05@users.noreply.github.com>
2024-03-10 22:14:53 +0100

    commento ScrittorePilota.java

--- src/main/java/com/mycompany/gran_prix/ScrittorePilota.java
+++ src/main/java/com/mycompany/gran_prix/ScrittorePilota.java
@@ -24,15 +24,21 @@ public class ScrittorePilota implements Runnable {
     }
 
     public void scrivi() {
-
+        // Crea un oggetto BufferedWriter per scrivere nel file 
         try (BufferedWriter br = new BufferedWriter(new FileWriter(nomeFile3))) {
+            // Scrive il nome del pilota nel file, racchiuso tra '<' e '>'.
             br.write("<" + nomePilota + ">");
+            // Aggiunge un carattere di nuova riga al file.
             br.write("\n\r");
+            // Scrive il modello dell'auto nel file, racchiuso tra '<' e '>'.
             br.write("<" + modelloAuto + ">");
+            // Aggiunge un carattere di nuova riga al file.
             br.write("\n\r");
+            // Forza la scrittura di tutti i dati nel buffer sul file.
             br.flush();
 
         } catch (IOException ex) {
+            // In caso di errore di input/output (IOException), registra l'errore usando un Logger.
             Logger.getLogger(ScrittorePilota.class.getName()).log(Level.SEVERE, null, ex);
         }
 
