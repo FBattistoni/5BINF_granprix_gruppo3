79d200e488db1ea3776cc00a6463246eb8dbe614
Marche05 <152077304+Marche05@users.noreply.github.com>
2024-03-10 22:12:11 +0100

    commento ScrittoreAuto.java

--- src/main/java/com/mycompany/gran_prix/ScrittoreAuto.java
+++ src/main/java/com/mycompany/gran_prix/ScrittoreAuto.java
@@ -19,12 +19,17 @@ public class ScrittoreAuto implements Runnable {
     }
 
     public void scrivi() {
+        // Crea un oggetto BufferedWriter per scrivere nel file
         try (BufferedWriter br = new BufferedWriter(new FileWriter(nomeFile2))) {
+            // Scrive il modello dell'auto racchiuso tra '<' e '>'
             br.write("<" + modelloAuto + ">");
+            // Aggiunge un carattere di nuova riga
             br.write("\n\r");
+            // Forza la scrittura di tutti i dati nel buffer sul file
             br.flush();
 
         } catch (Exception ex) {
+            // Stampa un messaggio di errore se si verifica un problema
             System.out.println("Errore: " + ex);
         }
     }
