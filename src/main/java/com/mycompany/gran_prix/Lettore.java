4a45493e47b95501b0a35e64c4a85e3065accd68
Marche05 <152077304+Marche05@users.noreply.github.com>
2024-03-10 22:34:39 +0100

    commento Lettore.java

--- src/main/java/com/mycompany/gran_prix/Lettore.java
+++ src/main/java/com/mycompany/gran_prix/Lettore.java
@@ -9,23 +9,28 @@ import java.io.FileReader;
  */
 public class Lettore extends Thread {
 
+    // Nome del file da leggere
     String nomeFile;
 
+    // Costruttore
     public Lettore(String nomeFile) {
         this.nomeFile = nomeFile;
     }
 
+    // Metodo per la lettura del file (carattere per carattere)
     public void leggi() {
-        synchronized (nomeFile) {
+        synchronized (nomeFile) { // Sincronizzazione per evitare letture multiple in contemporanea
             int i;
             try (FileReader fr = new FileReader(nomeFile)) {
+                // Lettura carattere per carattere dal file
                 while ((i = fr.read()) != -1) {
                     System.out.println((char) i);
                 }
 
-                System.out.println("\n\r");
+                System.out.println("\n\r"); // Aggiunge un carattere di nuova riga al file.
 
             } catch (IOException ex) {
+                // Gestione dell'errore in caso di problemi durante la lettura
                 System.err.println("Errore in lettura!");
             }
         }
